55c56a5b299bde6ab5f8df047a37983bbb802df2
Noland Seigler <nolandseigler@gmail.com>
2020-02-19 16:16:22 -0600

    delete exercise started

--- album_seeder.sql
+++ album_seeder.sql
@@ -1,4 +1,5 @@
 USE codeup_test_db;
+TRUNCATE albums;
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 47.3, 'pop, post-disco, funk, rock'),
        ('AC/DC', 'Back in Black', 1980, 29.4, 'hard rock'),
