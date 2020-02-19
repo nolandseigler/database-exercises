036d6cabc2a29e246dbc149023e77936910d0c77
Noland Seigler <nolandseigler@gmail.com>
2020-02-19 15:02:36 -0600

    building album seeder

--- albums_migration.sql
+++ albums_migration.sql
@@ -2,13 +2,15 @@ CREATE DATABASE IF NOT EXISTS codeup_test_db;
 USE codeup_test_db;
 DROP TABLE IF EXISTS albums;
 CREATE TABLE albums (
-    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR(50) DEFAULT 'NONE',
-    name VARCHAR(100) NOT NULL,
-    release_date INT UNSIGNED NOT NULL,
-    sales FLOAT UNSIGNED NOT NULL,
-    genre VARCHAR(100) NOT NULL,
-    PRIMARY KEY (id)
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50) DEFAULT 'NONE',
+                        name VARCHAR(100) NOT NULL,
+                        release_date INT UNSIGNED NOT NULL,
+                        sales FLOAT UNSIGNED NOT NULL,
+                        genre VARCHAR(100) NOT NULL,
+                        PRIMARY KEY (id)
 );
 DESCRIBE albums;
 SHOW CREATE TABLE albums;
+
+
