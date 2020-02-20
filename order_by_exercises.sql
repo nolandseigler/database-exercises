aac4a77183a6d5920bde4df4f7de03adc9590302
Noland Seigler <nolandseigler@gmail.com>
2020-02-20 16:25:49 -0600

    order_by_exercises complete

---
+++ order_by_exercises.sql
@@ -0,0 +1,15 @@
+use employees;
+SELECT * FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    OR first_name = 'Maya')
+  AND gender = 'M'
+ORDER BY last_name DESC, first_name DESC;
+SELECT * FROM employees
+WHERE last_name LIKE '%E'
+   OR last_name LIKE 'E%'
+ORDER BY emp_no DESC;
+SELECT * FROM employees
+WHERE last_name LIKE 'E%E';
+SELECT * FROM (SELECT * FROM employees WHERE birth_date LIKE '%-12-25' AND (hire_date BETWEEN '1990-01-01' and '2000-01-01')) AS t1 ORDER BY birth_date, hire_date DESC;
+SELECT * FROM (SELECT * FROM employees WHERE last_name LIKE '%q%') AS t1 WHERE last_name NOT LIKE '%qu%';
