bc1c36256f1c1464eba782ea4bbfdc7823642cd1
Noland Seigler <nolandseigler@gmail.com>
2020-02-24 11:19:38 -0600

    bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,6 +1,7 @@
 use employees;
 SHOW TABLES;
-SELECT * FROM salaries;
+SELECT *
+FROM dept_manager;
 
 # SELECT CURRENT MANAGER OF EVERY DEPARTMENT
 SELECT d.dept_name as Department, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager
@@ -10,7 +11,8 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = de.dept_no
          JOIN dept_manager as dm on de.emp_no = dm.emp_no
-WHERE dm.to_date > current_date() ORDER BY d.dept_name;
+WHERE dm.to_date > current_date()
+ORDER BY d.dept_name;
 
 # SELECT ALL DEPARTMENTS MANAGED BY FEMALES
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name) AS full_name
@@ -20,7 +22,9 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = de.dept_no
          JOIN dept_manager as dm on de.emp_no = dm.emp_no
-WHERE dm.to_date < current_date() AND e.gender = 'F' ORDER BY d.dept_name;
+WHERE dm.to_date < current_date()
+  AND e.gender = 'F'
+ORDER BY d.dept_name;
 
 # SELECT TITLES AND COUNT FROM CUSTOMER SERVICE
 SELECT t.title as Title, COUNT(*) AS Count
@@ -30,10 +34,12 @@ FROM employees as e
          JOIN departments as d
               ON d.dept_no = de.dept_no
          JOIN titles as t on de.emp_no = t.emp_no
-WHERE t.to_date > current_date() AND d.dept_name = 'Customer Service' GROUP BY t.title;
+WHERE t.to_date > current_date()
+  AND d.dept_name = 'Customer Service'
+GROUP BY t.title;
 
 # SELECT SALARY OF CURRENT MANAGERS
-SELECT d.dept_name as Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Name, s.salary
+SELECT d.dept_name as Department_Name, CONCAT(e.first_name, ' ', e.last_name) AS Name, s.salary as Salary
 FROM employees as e
          JOIN dept_emp as de
               ON de.emp_no = e.emp_no
@@ -41,4 +47,18 @@ FROM employees as e
               ON d.dept_no = de.dept_no
          JOIN dept_manager as dm on de.emp_no = dm.emp_no
          JOIN salaries as s on dm.emp_no = s.emp_no
-WHERE dm.to_date > current_date() AND s.to_date > current_date() ORDER BY d.dept_name;
+WHERE dm.to_date > current_date()
+  AND s.to_date > current_date()
+ORDER BY d.dept_name;
+
+# Bonus Find the names of all current employees, their department name, and their current manager's name.
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee_Name, d.dept_name as Department_Name, CONCAT(f.first_name, ' ', f.last_name) AS Manager_Name
+FROM employees as e
+         JOIN dept_emp as de ON e.emp_no = de.emp_no
+         JOIN departments d ON de.dept_no = d.dept_no
+         JOIN dept_manager dm ON de.dept_no = dm.dept_no
+         JOIN employees f ON dm.emp_no = f.emp_no
+WHERE dm.to_date > current_date()
+  AND de.to_date > current_date()
+ORDER BY d.dept_name;
+
