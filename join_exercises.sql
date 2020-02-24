40f0eabcaba753cc4ce64b0077bf0a2e1b9738f8
Noland Seigler <nolandseigler@gmail.com>
2020-02-24 12:07:07 -0600

    .

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,17 +1,14 @@
 use employees;
 SHOW TABLES;
 SELECT *
-FROM dept_manager;
+FROM employees;
 
 # SELECT CURRENT MANAGER OF EVERY DEPARTMENT
 SELECT d.dept_name as Department, CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager
 FROM employees as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
-         JOIN departments as d
-              ON d.dept_no = de.dept_no
-         JOIN dept_manager as dm on de.emp_no = dm.emp_no
-WHERE dm.to_date > current_date()
+         JOIN dept_emp as de ON de.emp_no = e.emp_no
+         JOIN dept_manager as dm ON de.emp_no = dm.emp_no AND dm.to_date > current_date()
+         JOIN departments as d ON dm.dept_no = d.dept_no
 ORDER BY d.dept_name;
 
 # SELECT ALL DEPARTMENTS MANAGED BY FEMALES
@@ -52,7 +49,9 @@ WHERE dm.to_date > current_date()
 ORDER BY d.dept_name;
 
 # Bonus Find the names of all current employees, their department name, and their current manager's name.
-SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee_Name, d.dept_name as Department_Name, CONCAT(f.first_name, ' ', f.last_name) AS Manager_Name
+SELECT CONCAT(e.first_name, ' ', e.last_name) AS Employee_Name,
+       d.dept_name                            as Department_Name,
+       CONCAT(f.first_name, ' ', f.last_name) AS Manager_Name
 FROM employees as e
          JOIN dept_emp as de ON e.emp_no = de.emp_no
          JOIN departments d ON de.dept_no = d.dept_no
