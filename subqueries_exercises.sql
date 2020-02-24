de4b0731ad3ffb2aa26a04f4e424f6ac066ee5d1
Noland Seigler <nolandseigler@gmail.com>
2020-02-24 12:45:20 -0600

    bonuses complete

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,5 +1,7 @@
 use employees;
+DESCRIBE departments;
 DESCRIBE dept_manager;
+DESCRIBE salaries;
 
 # Find all the employees with the same hire date as employee 101010 using a subquery.
 SELECT first_name, last_name
@@ -17,7 +19,7 @@ WHERE emp_no IN (
     SELECT emp_no
     FROM employees
     WHERE first_name = 'Aamod'
-) GROUP BY title
+) GROUP BY title;
 
 # Find all the current department managers that are female.
 SELECT first_name, last_name
@@ -26,4 +28,34 @@ WHERE emp_no IN (
     SELECT emp_no
     FROM dept_manager
     WHERE dept_manager.to_date > current_date()
-    ) AND gender = 'F'
+    ) AND gender = 'F';
+
+# BONUS Find all the department names that currently have female managers.
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+        SELECT emp_no
+        FROM employees
+        WHERE emp_no IN (
+            SELECT emp_no
+            FROM dept_manager
+            WHERE dept_manager.to_date > current_date()
+        )
+        AND gender = 'F'
+    )
+);
+
+# BONUS Find the first and last name of the employee with the highest salary
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM salaries
+    WHERE salary IN (
+        SELECT MAX(salary)
+        FROM salaries
+    )
+);
